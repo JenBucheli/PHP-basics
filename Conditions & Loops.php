ee93a4a2c02b6e32416542237bc091c9f89051b3
Jen Bucheli <you@example.com>
2021-03-03 21:02:35 +0100

    renamed:    conditonals.php -> Conditions & Loops.php 	new file:   Exercise about functions.php 	deleted:    switch.php

--- Conditions & Loops.php
+++ Conditions & Loops.php
@@ -18,17 +18,18 @@ if($intTwo==$intThree){
     echo 'both numbers are equal';
 }
 
-//why when there are two separate conditions it just prints the result of one?
-// Why when they all are one scope I don't get the result if two of them are true.
+//why when there are two separate conditions it just prints the result of one? TODO
+// Why when they all are one scope I don't get the result if two of them are true.TODO
 
-//Often you'd want to have more than one statement to be executed conditionally. Of course, there's no need to wrap each statement with an if clause. Instead, you can group several statements into a statement group. For example, this code would display a is bigger than b if $a is bigger than $b, and would then assign the value of $a into $b:
+//Often we want to have more than one statement to be executed conditionally. We can group several statements into a statement group. For example,
+// this code would display a is bigger than b if $a is bigger than $b, and would then assign the value of $a into $b:
 
 $a=5;
 $b=2;
 if ($a > $b) {
 
     echo "<br> a is bigger than b <br>";
-     //$b = $a; when I assign new values to the variables the results from under don't display anymore in the browser. why?
+     //$b = $a; TODO//when I assign new values to the variables the results from under don't display anymore in the browser. why? TODO
     echo "<br>$b = $a";
 }
 
